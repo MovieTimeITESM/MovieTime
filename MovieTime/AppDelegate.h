447f434785d8c9951f17ffe1e356948c147bae6a
PatoBeltran <pbelsal@gmail.com>
2014-10-20 12:29:54 -0500

    Initial Commit

---
+++ MovieTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieTime
+//
+//  Created by Patricio Beltrán on 10/20/14.
+//  Copyright (c) 2014 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieTime
+//
+//  Created by Patricio Beltrán on 10/20/14.
+//  Copyright (c) 2014 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
